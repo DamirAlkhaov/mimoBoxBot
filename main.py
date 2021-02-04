8283c64cce2c634ed9c78400d617ee6dc0527dae
Damir Alkhaov <alkhaovdamirgt@gmail.com>
2021-02-04 19:59:45 +0000

    fixed status now shows active and not idle, pings user in messages

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ client = discord.Client()
 async def on_ready():
   print('logged in as {0.user}'.format(client))
   activity = discord.Game(name="Listening for mb!help")
-  await client.change_presence(status=discord.Status.idle, activity=activity)
+  await client.change_presence(status=discord.Status.online, activity=activity)
 
 @client.event
 async def on_message(message):
@@ -15,7 +15,7 @@ async def on_message(message):
     return
   
   if message.content.startswith('mb!info'):
-    user = 'This is MimoBox Bot! Thank you for asking ' + str(message.author)
+    user = 'This is MimoBox Bot! Thank you for asking @' + str(message.author)
     await message.channel.send(user)
 
   if message.content.startswith('mb!help'):
@@ -26,7 +26,7 @@ async def on_message(message):
     
     '```')
   if message.content.startswith('mb!love'):
-    user = 'I love you alot ' + str(message.author) + '!'
+    user = 'I love you alot @' + str(message.author) + '!'
     await message.channel.send(user)
 
 client.run(os.getenv('TOKEN'))
